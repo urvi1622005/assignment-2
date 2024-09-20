7802878c9d5362514d93a2debc8db5facceef4b2
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-09-20 23:49:20 +0530

    game

---
+++ game.java
@@ -0,0 +1,17 @@
+public class game {
+    /**
+     * The main method is the entry point for the game.
+     * 
+     * It takes in an array of strings as arguments, which are the command line
+     * arguments passed in when the program is run. The arguments are not used in
+     * this program.
+     */
+    public static void main(String[]args){
+
+        System.out.println("Hello World");
+        
+        
+
+    }
+    
+}
