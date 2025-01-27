937080186b7bf06be9c952a62c9150a250f12bf5
urvi1622005 <urvashidhawan1622005@gmail.com>
2025-01-27 15:27:34 +0530

    ...

---
+++ random/armstrongNumber.java
@@ -0,0 +1,23 @@
+package random;
+import java.util.Scanner;
+public class armstrongNumber {
+
+    public static void main(String[] args) {    
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        scanner.close();
+        int temp = num, sum = 0, n = Integer.toString(num).length();
+        while (temp != 0) {
+            int digit = temp % 10;
+            sum += Math.pow(digit, n);
+            temp /= 10;
+        }
+        if (sum == num) {
+            System.out.println(num + " is an Armstrong number.");
+        } else {
+            System.out.println(num + " is not an Armstrong number.");
+        }
+    }
+    
+}
