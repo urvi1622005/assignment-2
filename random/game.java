89891454f18b82b13846e8001c56bdcb42e65178
urvi1622005 <urvashidhawan1622005@gmail.com>
2025-01-27 15:28:31 +0530

    ...

---
+++ random/game.java
@@ -0,0 +1,18 @@
+package random;
+import java.lang.*;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class game {
+    public static void main(String[] args) throws IOException {
+        
+        //your code goes here
+        BufferedReader reader =new BufferedReader(new InputStreamReader(System.in));
+        int Read = Integer.parseInt(reader.readLine());
+        BufferedReader reade =new BufferedReader(new InputStreamReader(System.in));
+        int Reade = Integer.parseInt(reader.readLine());
+
+        System.out.print(Read);
+    }
+}
