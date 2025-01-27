6d1d72bdf78b7f5711079a0d15c1e6b27484817a
urvi1622005 <urvashidhawan1622005@gmail.com>
2025-01-27 15:28:10 +0530

    ..

---
+++ Leetcode/MaximumSubarray.java
@@ -0,0 +1,20 @@
+package Leetcode;
+public class MaximumSubarray {
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int maxSum = maxSubArray(nums);
+        System.out.println("Maximum Subarray Sum: " + maxSum);
+    }
+
+    public static int maxSubArray(int[] nums) {
+        int maxSoFar = nums[0];
+        int maxEndingHere = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            maxEndingHere = Math.max(nums[i], maxEndingHere + nums[i]);
+            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+        }
+
+        return maxSoFar;
+    }
+}
